608cbc3577de3bece7b8008176cd260f870e59e8
changeonPark <qkrcksrjs@gmail.com>
2023-03-13 17:29:21 +0900

    배경 색, 투명도 설정

--- src/basic.js
+++ src/basic.js
@@ -11,17 +11,22 @@ function setSize(camera, scene, renderer) {
 export default function example() {
   const canvas = document.getElementById("three-canvas");
 
-  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
 
+  renderer.setClearColor("#00ff00", 1.0);
+  renderer.setClearAlpha(0.5);
+
   const scene = new THREE.Scene();
 
+  scene.background = new THREE.Color("#000000"); // scene >> renderer
+
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 1;
-  camera.position.y = 2;
-  camera.position.x = 5;
+  camera.position.z = 5;
+  camera.position.y = 1.5;
+  camera.position.x = 1;
 
   // const camera = new THREE.OrthographicCamera(
   //   -(window.innerWidth / window.innerHeight), // left
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 // import example01 from "./ex01.js";
-import example from "./ex03";
+import example from "./basic";
 
 example();
